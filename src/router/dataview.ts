49df6ff4210406858337d57c795e0ae2d4337193
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-01-22 11:46:44 +0800

    执行 Dataview 代码并返回结果

--- src/router/dataview.ts
+++ src/router/dataview.ts
@@ -1,16 +1,24 @@
 /* Obsidian 插件交互：Dataview */
 
 import Router from '@koa/router'
+import { bodyParser } from '@koa/bodyparser'
 import { getAPI } from 'obsidian-dataview'
 
 // 注：Dataview 是数字桌搭与 Obsidian 交互的核心插件，访问路径将直接用 /dataview 而非 /plugin/dataview
 const routerDataview = new Router({ prefix: '/dataview' })
-const dvapi = getAPI()
+routerDataview.use(bodyParser())
+
+const dv = getAPI()
 
 
 routerDataview.get('/page/:relpath*', (ctx, next) => {
     const relpath = ctx.params.relpath
-    ctx.body = dvapi.page(relpath) || ''
+    ctx.body = dv.page(relpath) || ''
+})
+
+routerDataview.post('/query', async (ctx, next) => {
+    const source = ctx.request.body?.source
+    ctx.body = await dv.query(source)
 })
 
 
