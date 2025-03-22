7b0a189dd35c4014fd71653c814a8e204addf905
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-03-22 13:36:22 +0800

    Obsidian：新增接口 current-vault 当前仓库

--- src/router/obsidian.ts
+++ src/router/obsidian.ts
@@ -1,8 +1,10 @@
 /* Obsidian 本体交互 */
-
-import Router from '@koa/router'
 import { App } from 'obsidian'
 
+import Router from '@koa/router'
+
+// 类型注解
+import { FileSystemAdapter } from 'obsidian'
 
 export default class Obsidian {
     router: Router
@@ -13,6 +15,15 @@ export default class Obsidian {
         this.app = app
 
         // 路由注册
+        this.router.get('/current-vault', async (ctx, next) => {
+            const vaultpath = (this.app.vault.adapter as FileSystemAdapter).getBasePath()
+            const vaultname = this.app.vault.getName()
+            ctx.body = {
+                vaultpath: vaultpath || '',
+                vaultname: vaultname || ''
+            }
+        })
+
         this.router.get('/current-note', (ctx, next) => {
             const relpath = this.app.workspace.getActiveFile()?.path
             ctx.body = relpath || ''
