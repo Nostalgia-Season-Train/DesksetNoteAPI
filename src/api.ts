29f6e5588c41df3297af1253d7f5095219604866
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-04-14 23:58:03 +0800

    Koa 新增：check_access 中间件，仅放行本机访问

--- src/api.ts
+++ src/api.ts
@@ -26,6 +26,7 @@ export default class DesksetNoteAPI {
         this.note = new Note(this.app)
 
         this.server = new Koa()
+        this.server.use(this.check_access)
         this.server.use((new Obsidian(this.app)).routes())
         this.server.use(routerDataview.routes())
         this.server.use((new Diary(this.app, this.note)).router.routes())
@@ -33,6 +34,14 @@ export default class DesksetNoteAPI {
         this.server.use((new Stats(this.app)).router.routes())
 
         this.listen = null
+    }
+
+    check_access = async (ctx: any, next: any): Promise<void> => {
+        // 仅允许本机访问
+        if (ctx.req.socket.remoteAddress != '127.0.0.1') {
+            ctx.throw(403, 'Access denied. Only requests from 127.0.0.1 are allowed.')
+        }
+        await next()
     }
 
     open(host: string, port: number) {
