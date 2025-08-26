0b97f7d785303c39934679f8864a3d59f7a2b3fd
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-08-26 10:41:45 +0800

    Unify 新增：获取热力图 get_heatmap

--- src/unify.ts
+++ src/unify.ts
@@ -1,4 +1,4 @@
-import { App } from 'obsidian'
+import { App, moment } from 'obsidian'
 import { DataviewApi, getAPI } from 'obsidian-dataview'
 
 export default class Unify {
@@ -34,6 +34,32 @@ export default class Unify {
         const oldestNote = this._dataviewApi.pages().file.sort(file => file.cday)[0]  // 比较 cday 而不是 ctime 节省性能
         // @ts-ignore
         return Math.floor([new Date() - oldestNote.ctime] / (60 * 60 * 24 * 1000))
+    }
+
+    // 获取热力图：统计本周 + 前 weeknum 周的笔记创建和编辑数量
+    get_heatmap = async (weeknum: number): Promise<{ date: string, number: number }[]> => {
+        // 起始日期 ~ 结束日期：前 weeknum 周周一 ~ 今天
+          // 注：不会统计同一笔记，连续编辑的情况...除非追踪 Git
+        const start = Number(moment().subtract(weeknum, 'weeks').startOf('week').format('YYYYMMDD'))
+        const end = Number(moment().format('YYYYMMDD'))
+
+        // 所有笔记创建日期和修改日期的数组
+        let ctimes: number[] = []
+        let mtimes: number[] = []
+        this._dataviewApi.pages().map((page: any) => {
+            ctimes.push(Number(moment(page.file.ctime.ts).format('YYYYMMDD')))
+            mtimes.push(Number(moment(page.file.mtime.ts).format('YYYYMMDD')))
+        })
+
+        // 热点 = { 日期, 当日创建和编辑笔记的数量 }
+        let heats: Array<{ date: string, number: number }> = []
+
+        for (let date = start; date <= end; date = Number(moment(date, 'YYYYMMDD').add(1, 'days').format('YYYYMMDD'))) {
+            const number = ctimes.filter(ctime => ctime == date).length + mtimes.filter(mtime => mtime == date).length
+            heats.push({ date: String(date), number: number })
+        }
+
+        return heats
     }
 
     // 获取活跃文件（当前聚焦的标签页）
