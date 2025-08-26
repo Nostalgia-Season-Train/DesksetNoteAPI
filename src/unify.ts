2b7fd120f7d798015924db50a21ef66424dcc40c
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-08-26 15:28:47 +0800

    Unify 新增：返回查询建议 suggest_by_switcher

--- src/unify.ts
+++ src/unify.ts
@@ -1,9 +1,13 @@
 import { App, moment } from 'obsidian'
 import { DataviewApi, getAPI } from 'obsidian-dataview'
 
+import Suggest from './feature/suggest'
+
 export default class Unify {
     private _app: App
     private _dataviewApi: DataviewApi
+
+    private _suggest: Suggest
 
     constructor(app: App) {
         this._app = app
@@ -12,6 +16,8 @@ export default class Unify {
         if (this._dataviewApi == undefined) {
             throw Error('Dataview not enable')
         }
+
+        this._suggest = new Suggest(this._app)
     }
 
     // 获取笔记总数
@@ -65,5 +71,20 @@ export default class Unify {
     // 获取活跃文件（当前聚焦的标签页）
     get_active_file = async (): Promise<string> => {
         return this._app.workspace.getActiveFile()?.path ?? ''
+    }
+
+    // 返回查询建议，数据来源：核心插件/快速切换
+    suggest_by_switcher = async (query: string) => {
+        return (await this._suggest.getSuggestions(query)).map(item => {
+            if (item?.type != 'file')
+                return null
+            if (item?.file?.basename == undefined || item?.file?.extension == undefined || item?.file?.path == undefined)
+                return null
+            return {
+                name: item.file.basename,
+                type: item.file.extension,
+                path: item.file.path
+            }
+        }).filter(item => item != null)
     }
 }
